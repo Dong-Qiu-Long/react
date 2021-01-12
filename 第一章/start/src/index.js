f896e9277a33cfbc3244561b6b7805e374b28128
Dong-Qiu-Long <1902941357@qq.com>
2021-01-12 14:08:52 +0800

    react

--- 第一章/start/src/index.js
+++ 第一章/start/src/index.js
@@ -1,46 +1,19 @@
-import React from 'react';
+import React , {useContext}from 'react';
 import ReactDOM from 'react-dom';
 
-function ChildA(){
-	const dom = (
-	<div>
-		<h1>ChildA</h1>
-		<ChildB></ChildB>
-	</div>
-	);
-	return ReactDOM.createPortal(dom,document.querySelector('#main'))
+const ctx = React.createContext();
+
+function Test(){
+	const value = useContext(ctx)
+	return <h1>{value}</h1>
 }
-//错误处理类组件
-class Err extends React.PureComponent{
-	state = {
-		hasError:false
-	}
-	// static getDerivedStateFromError(err){
-	// 	console.log('发生了错误',err)
-	// 	return {
-	// 		hasError:true
-	// 	}
-	// }
-	componentDidCatch(err,info){
-		console.log('发生错误')
-		this.setState({
-			hasError:true
-		})
-	}
-	render(){
-		if(this.state.hasError){
-			return <h1>发生了错误</h1>
-		}
-		return this.props.children
-	}
-}
-function App(){
+function Func(){
 	return(
-		<div className="app">
-		<Err>
-			<ChildA/>
-		</Err>
+		<div>
+			<ctx.Provider value="456">
+				<Test/>
+			</ctx.Provider>
 		</div>
 	)
 }
-ReactDOM.render(<div><App/></div>,document.getElementById('root'));
+ReactDOM.render(<div><Func/></div>,document.getElementById('root'));
