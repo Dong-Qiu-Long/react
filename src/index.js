1967926b90647325eb34f3e18b7e2504d7e66c34
Dong-Qiu-Long <1902941357@qq.com>
2021-01-19 00:50:01 +0800

    事件

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Ball from './poll.js'
 
-function getRundom(min,max){
-  return parseInt(Math.random()*(max-min)) + min;
-}
-function getRundomGg(){
-  return '#'+Math.random().toString(16).substr(2,3)
-}
-function Polls(props){
-  console.log(getRundom(0,50))
-  const arr = []
-   for(var i = 0; i < props.num; i++){
-    arr.push(<Ball key={i} x = {getRundom(90,190)} y = {getRundom(90,190)} left = {getRundom(0,900)} top = {getRundom(0,900)} bg={getRundomGg()}/>)
-   }
-   return <div>{arr}</div>
+class NumBtn extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      num:0
+    }
+  }
+  render(){
+    return <div>
+      <button onClick={()=>{
+        this.setState({
+          num:this.state.num -1
+        })
+      }}>减</button>
+      <p>{this.state.num}</p>
+      <button onClick={()=>{
+        this.setState({
+          num:this.state.num + 1
+        })
+      }}>加</button>
+    </div>
+  }
 }
 
-ReactDOM.render(<div><Polls num={10}/></div>,
+
+ReactDOM.render(<div><NumBtn/></div>,
   document.getElementById('root')
 );
 
