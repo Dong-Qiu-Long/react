f8bf5ef399a5bbeab3d82491f2c3dd5db550be3c
Dong-Qiu-Long <1902941357@qq.com>
2021-01-18 20:24:36 +0800

    练习  图片切换

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import img1 from './time.jpg';
+import img2 from './family.jpg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//图片数组
+const arrImg = [img1,img2];
+//计数器
+let index = 0;
+//定时器
+let times = null;
+// 开始轮播
+function timeFun(){
+  times = setInterval(function(){
+    index++
+    const div = (<div style={{
+      width:500,
+      height:400,
+      border:'1px solid #ccc'
+    }}
+    onMouseLeave={()=>{
+      console.log('开始了');
+      timeFun()
+    }}
+    onMouseOver = {()=>{
+      console.log('停止了');
+      clearInterval(times)
+    }}
+    >
+      <img style={{
+        width:'100%'
+      }} src={arrImg[index%2]} alt=""/>
+    </div>)
+    ReactDOM.render(div,
+      document.getElementById('root')
+    );
+  },2000)
+}
+timeFun()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
