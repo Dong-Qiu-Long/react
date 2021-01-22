4ae4082dacfa684add6574a04c88f7a750dde403
Dong-Qiu-Long <1902941357@qq.com>
2021-01-22 13:20:49 +0800

    Protals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React, { Component,PureComponent } from 'react'
 import ReactDOM from 'react-dom';
-import Son from './Son'
-import Diamond from './Diamond';
-ReactDOM.render(<div><Son/><Diamond/></div>,
+
+function ChildA(){
+  return <div className="child-a">
+    我是A
+    <ChildB/>
+  </div>
+}
+function ChildB(){
+  const div =  <div className="child-b">
+    我是B
+  </div>
+  return ReactDOM.createPortal(div,document.querySelector('#main'))
+}
+
+function App(){
+  return (
+    <div className="app">
+      我是App
+      <ChildA/>
+    </div>
+  )
+}
+
+ReactDOM.render(<div><App/></div>,
   document.getElementById('root')
 );
 
