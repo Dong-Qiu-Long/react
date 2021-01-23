8c02c04bcd66cc6847079b3660d8a73d6b94448a
Dong-Qiu-Long <1902941357@qq.com>
2021-01-23 10:43:15 +0800

    自定义Hook

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import React, { Component,PureComponent ,useState } from 'react'
+import React, {useState,useEffect } from 'react'
 import ReactDOM from 'react-dom';
+import GetStudent from './hook';
 
 function App(){
-  const [num, setNum] = useState(0);
-  const [self,setSelf] = useState(true)
-  return (
-  <div>
-    <button onClick={()=>{
-      setNum(num+1)
-    }}>加</button>
-    <h1>{num}</h1>
-    <button onClick={()=>{
-      setNum(num => num-1)
-      setNum(num => num-1)
-    }}>减</button>
-    <h1 style = {{
-      display:self ? 'block':'none'
-    }}>你说咋地就咋地</h1>
-    <button onClick={()=>{
-      setSelf(!self)
-    }}>显示/隐藏</button>
-  </div>)
+  const data = GetStudent();
+  const list = data.map((it,i) => {
+    return <li key={i}>姓名: {it.name} 年龄: {it.age}</li>
+  })
+  return <div>
+    <ul>
+      {list}
+    </ul>
+  </div>
 }
 
 ReactDOM.render(<App/>,
@@ -28,23 +19,14 @@ ReactDOM.render(<App/>,
 );
 
 /**
- * HOOK简介
- * 组件:无状态组件， 类组件
- * 
- * 类组件中的麻烦
- * 1.this指向问题
- * 2.繁琐的生命周期
- * 
- * HOOK专门用于增强函数组件的功能，使之理论上可以成为类组件的替代品
- * HOOK(钩子) 本质上是一个函数， 该函数可以挂在任何功能
- * HOOK种类
- * 1.useState
- * 2.useEffect
- * 3.其他。。。
- * 
+ * 自定义HOOOK
+ * State Hook: useState
+ * Effect HOOK: useEffect
  */
-
- /**
-  * 一个函数组件中可以有多个状态
-  * 这种做法非常有利于横切关注点
-  */
+/**
+ * 自定义HOOOK
+ * 自定义Hook:将一些常用的，跨越多个组件的Hook功能，抽离出去形成一个函数，该函数就是自定义hook
+ * 自定义Hook功能，所以它本省也需要按照Hook的规则实现
+ * 例如:
+ * 1.很多组价你都需要在第一次加载完成够，获取所有学生数据
+ */
